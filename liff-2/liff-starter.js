0590d44e0ab0634db95eea5cdd77858191765401
sozi-line <zahranissa820@gmail.com>
2022-12-05 16:49:07 +0700

    Initial commit

---
+++ liff-2/liff-starter.js
@@ -0,0 +1,84 @@
+// window.onload = function() {
+//     myLiffId = "1655208881-R0boqDXk";
+//     initializeLiff(myLiffId);
+// };
+
+function initializeLiff(myLiffId) {
+    liff
+        .init({
+            liffId: myLiffId
+        })
+        .then(() => {
+            sendLiff();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function sendLiff(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'text') {
+        liff.sendMessages([{
+            type: 'text',
+            text: getParameterByName('text'),
+            sentBy: {
+                label: "© SOZI BOTS",
+                iconUrl: "https://i.ibb.co/Yt8PxKh/3hn0N.gif",
+                linkUrl: "http://line.me/ti/p/~arshleo"
+            }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'image') {
+        liff.sendMessages([{
+            type: 'image',
+            originalContentUrl: getParameterByName('img'),
+            previewImageUrl: getParameterByName('img'),
+            sentBy: {
+                label: "© SOZI BOTS",
+                iconUrl: "https://i.ibb.co/Yt8PxKh/3hn0N.gif",
+                linkUrl: "http://line.me/ti/p/~arshleo"
+            }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'video') {
+        prev = getParameterByName('piu');
+        if(prev !== null && prev !== '') {
+            dura = prev;
+        } else {
+            dura = "https://s6.gifyu.com/images/2d02a745c078f037322f577fa25b7c38.gif";
+        }
+        liff.sendMessages([{
+            type: 'video',
+            originalContentUrl: getParameterByName('ocu'),
+            previewImageUrl: dura
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    } else if (tipe === 'audio') {
+        duration = getParameterByName('duration');
+        if(duration !== null && duration !== '') {
+            dura = parseInt(duration);
+        } else {
+            dura = 60000;
+        }
+        liff.sendMessages([{
+            type: 'audio',
+            originalContentUrl: getParameterByName('link'),
+            duration: dura
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
